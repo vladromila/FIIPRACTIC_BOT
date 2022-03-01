17e809f726f740c4aeaf9cf0db7e1c694c3bc6fb
Vlad Romila <vladromila@192-168-0-170.rdsnet.ro>
2022-03-01 19:31:36 +0200

    changed

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on("messageCreate", async message => {
 
     const command = message.content.trim();
     if (message.channelId == '946865824906510416') {
-
+        console.log(`https://fiicode-api.asii.ro/teams/code/${command}`);
         let team = await axios.get(`https://fiicode-api.asii.ro/teams/code/${command}`).catch(() => {
             return {}
         })
